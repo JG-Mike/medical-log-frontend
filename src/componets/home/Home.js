352f33ff7a8800084843845f284a17afe0f1135e
Michael Gichuhi <jmikekamau@gmail.com>
2023-03-08 16:02:57 +0300

    💄 started styling the home page having issues with the h1 and p tag layout

--- src/componets/home/Home.js
+++ src/componets/home/Home.js
@@ -3,13 +3,17 @@ import './Home.css'
 
 function Home() {
   return (
-    <div className="wrapper">
-      <h1>home</h1>
-      <img id='image' src="images/image1.jpg" alt="" />
-      <div className="text-box">
 
-      </div>
-    </div>
+
+  <div className="wrapper">
+    <img id='image' src="images/image1.jpg" alt="" />
+    <div className="text-box"></div>
+
+    <h2> nurses</h2>
+    <p>welcome to my website</p>
+  </div>
+  
+
   )
 }
 
