98d2c8959b720e104733ebbdc251277d951a289b
Michael Gichuhi <jmikekamau@gmail.com>
2023-03-09 10:14:49 +0300

    ✏️ 💄fixed the h1 and p tage problem which was a typo

--- src/componets/home/Home.js
+++ src/componets/home/Home.js
@@ -7,10 +7,11 @@ function Home() {
 
   <div className="wrapper">
     <img id='image' src="images/image1.jpg" alt="" />
-    <div className="text-box"></div>
+    <div className="text-box">
 
     <h2> nurses</h2>
     <p>welcome to my website</p>
+    </div>
   </div>
   
 
